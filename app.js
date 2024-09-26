8e4ab629e629935ae2c399fb24446e5fb1202d1c
bahrom <baxrom@email.com>
2024-09-26 19:01:20 +0500

    misolalr ready

---
+++ app.js
@@ -0,0 +1,165 @@
+//1-misol
+// let a = prompt("son kiriting");
+// if (a > 0) {
+//   console.log("a son musbat");
+// } else {
+//   console.log("a son musbat emas");
+// }
+
+// 2-misol
+// let a = prompt("son kiting");
+// if (a % 2 > 0) {
+//   console.log("a son toq");
+// } else {
+//   console.log("a son toq emas");
+// }
+
+//3-misol
+// let a = prompt("son kiriting");
+// if (a % 2 == 0) {
+//   console.log("a son juft");
+// } else {
+//   console.log("a son juft emas");
+// }
+
+//4-misol
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a > 2 && b <= 3) {
+//   console.log(" rost");
+// } else {
+//   console.log(" yolg'on");
+// }
+
+//5-misol
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a >= 0 && b < -2) {
+//   console.log(" rost");
+// } else {
+//   console.log(" yolg'on");
+// }
+
+//6-misol
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+// if (a <= b && b <= c && a <= c) {
+//   console.log(" rost");
+// } else {
+//   console.log(" yolg'on");
+// }
+
+//7-misol
+// let a = prompt("a sonini kiritung");
+// let b = prompt("b sonini kiritung");
+// let c = prompt("c sonini kiritung");
+// if ((a <= b && b <= c) || (c <= b && b <= a)) {
+//   console.log(" rost.");
+// } else {
+//   console.log(" yolg'on.");
+// }
+
+//8-misol
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if ((a % 2 > 0) & (b % 2 > 0)) {
+//   console.log("a va b sonlari toq");
+// } else {
+//   console.log("a va b sonlari toq emas");
+// }
+
+//9-misol
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a % 2 > 0 || b % 2 > 0) {
+//   console.log("sonlarining biri toq");
+// } else {
+//   console.log("sonlarining hec qaysisi toq emas");
+// }
+
+//10-misol
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a % 2 > 0 || b % 2 > 0) {
+//   console.log("sonlarining biri toq");
+// } else {
+//   console.log("sonlarining hec qaysisi toq emas");
+// }
+
+//11-misol
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if ((a % 2 === 0 && b % 2 === 0) || (a % 2 !== 0 && b % 2 !== 0)) {
+//   console.log(" rost");
+// } else {
+//   console.log(" yolg'on");
+// }
+
+//12-misol
+// let a = prompt("a sonini kiritung");
+// let b = prompt("b sonini kiritung");
+// let c = prompt("c sonini kiritung");
+// if (a > 0 && b > 0 && c > 0) {
+//   console.log(" rost.");
+// } else {
+//   console.log(" yolg'on.");
+// }
+
+//13-misol
+// let a = prompt("a sonini kiritung");
+// let b = prompt("b sonini kiritung");
+// let c = prompt("c sonini kiritung");
+// if (a > 0 || b > 0 || c > 0) {
+//   console.log(" rost.");
+// } else {
+//   console.log(" yolg'on.");
+// }
+
+//14-misol
+// let a = prompt("a sonini kiritung");
+// let b = prompt("b sonini kiritung");
+// let c = prompt("c sonini kiritung");
+// let positiveCount = 0;
+// if (a > 0) positiveCount++;
+// if (b > 0) positiveCount++;
+// if (c > 0) positiveCount++;
+// if (positiveCount === 2) {
+//   console.log(" rost, faqat ikkita son musbat.");
+// } else {
+//   console.log(" yolg'on, faqat ikkita son musbat emas.");
+// }
+/////////////////////////////////////////////////////////
+//1-misol
+// let number = prompt("son kiriting");
+// if (number > 0) {
+//   number++;
+// }
+// console.log(+number);
+
+//2-misol
+// let number = prompt("son kiriting");
+// if (number > 0) {
+//   number++;
+//   console.log(number);
+// } else if (number < 0) {
+//   number -= 2;
+//   console.log(number);
+// } else {
+//   console.log(" kiritilgan son nolga teng");
+// }
+
+//3-misol
+// let number = prompt("son kiriting");
+// if (number > 0) {
+//   number++;
+//   console.log(number);
+// } else if (number < 0) {
+//   number -= 2;
+//   console.log(number);
+// } else {
+//   number = 10;
+//   console.log(+number);
+// }
+
+// 4-misol
