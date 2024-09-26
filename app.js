d88994eb88d864215c0295d4f025304a4a8c7f85
bahrom <baxrom@email.com>
2024-09-26 21:45:07 +0500

    misol ready

--- app.js
+++ app.js
@@ -163,3 +163,115 @@
 // }
 
 // 4-misol
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+// let musbatlarSoni = 0;
+// if (a > 0) musbatlarSoni++;
+// if (b > 0) musbatlarSoni++;
+// if (c > 0) musbatlarSoni++;
+// console.log("Musbat sonlar soni: " + musbatlarSoni);
+
+//5-misol
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+// let musbatlarSoni = 0;
+// if (a > 0) musbatlarSoni++;
+// if (b > 0) musbatlarSoni++;
+// if (c > 0) musbatlarSoni++;
+// console.log("Musbat sonlar: " + musbatlarSoni);
+// let manfiylarSoni = 0;
+// if (a < 0) manfiylarSoni++;
+// if (b < 0) manfiylarSoni++;
+// if (c < 0) manfiylarSoni++;
+// console.log("Manfiy  sonlar: " + manfiylarSoni);
+
+//6-misol
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a > b) {
+//   console.log(a);
+// } else if (b > a) {
+//   console.log(b);
+// }
+
+//7-misol
+// let a = prompt("Birinchi butun sonni kiriting:");
+// let b = prompt("Ikkinchi butun sonni kiriting:");
+// let kichikSon, tartibRaqami;
+// if (a < b) {
+//   kichikSon = a;
+//   tartibRaqami = 1;
+// } else if (a > b) {
+//   kichikSon = b;
+//   tartibRaqami = 2;
+// } else {
+//   console.log("Ikkita son teng.");
+//   kichikSon = a;
+//   tartibRaqami = 0;
+// }
+// if (tartibRaqami > 0) {
+//   console.log("Kichik son: " + kichikSon + ", Tartib raqami: " + tartibRaqami);
+// }
+
+//8-misol
+// let a = prompt("Birinchi butun sonni kiriting:");
+// let b = prompt("Ikkinchi butun sonni kiriting:");
+// let kattaSon, kichikSon;
+// if (a > b) {
+//   kattaSon = a;
+//   kichikSon = b;
+// } else if (a < b) {
+//   kattaSon = b;
+//   kichikSon = a;
+// } else {
+//   console.log("Ikkita son teng: " + a);
+// }
+// if (kattaSon !== undefined && kichikSon !== undefined) {
+//   console.log("Katta son: " + kattaSon);
+//   console.log("Kichik son: " + kichikSon);
+// }
+
+//9-misol
+
+//10-misol
+// let a = prompt("a sonini kiriting:");
+// let b = prompt("b sonini kiriting:");
+// if (a !== b) {
+//   let yigindi = a + b;
+// } else {
+//   a = 0;
+//   b = 0;
+// }
+// console.log("a ning qiymati: " + a);
+// console.log("b ning qiymati: " + b);
+
+//11-misol
+// let a = prompt("a sonini kiriting:");
+// let b = prompt("b sonini kiriting:");
+// if (a !== b) {
+//   a = Math.max(a, b);
+//   b = a;
+// } else {
+//   a = 0;
+//   b = 0;
+// }
+// console.log("a ning qiymati: " + a);
+// console.log("b ning qiymati: " + b);
+
+//12-misol
+// let x = prompt("Birinci sonni kiriting:");
+// let y = prompt("Ikkinchi sonni kiriting:");
+// let z = prompt("Uchinchisi sonni kiriting:");
+// let kichikSon = Math.min(x, y, z);
+// console.log("Kichik son: " + kichikSon);
+
+//14-misol
+let a = prompt("Birinchi sonni kiriting:");
+let b = prompt("Ikkinchi sonni kiriting:");
+let c = prompt("Uchinchisi sonni kiriting:");
+let kichikSon = Math.min(a, b, c);
+let kattaSon = Math.max(a, b, c);
+console.log("Kichik son: " + kichikSon);
+console.log("Katta son: " + kattaSon);
